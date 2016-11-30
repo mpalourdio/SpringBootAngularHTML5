23cfe2660a2f7eb3b68c27dba2dec0d807175e31
Michel Palourdio <mpalourdio@gmail.com>
2016-11-30 23:15:24 +0100

    Add spring-security config to disable cache control & co.

---
+++ src/main/java/com/mpalourdio/html5/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.mpalourdio.html5.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.headers().defaultsDisabled().cacheControl();
+    }
+}
+

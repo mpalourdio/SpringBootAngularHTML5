50d92c87d867afa59981e865086442ef722e786d
Michel Palourdio <mpalourdio@gmail.com>
2017-01-16 20:43:36 +0100

    Disable csrf

--- src/main/java/com/mpalourdio/html5/config/WebSecurityConfig.java
+++ src/main/java/com/mpalourdio/html5/config/WebSecurityConfig.java
@@ -11,6 +11,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http.headers().defaultsDisabled().cacheControl();
     }
 }

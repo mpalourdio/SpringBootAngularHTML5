7d49bb81ac4c222b5a8bb0ccb8c707089a53297d
Michel Palourdio <mpalourdio@gmail.com>
2017-04-08 19:54:35 +0200

    Add MIT blocks

--- front/src/app/second/second.component.spec.ts
+++ front/src/app/second/second.component.spec.ts
@@ -4,6 +4,15 @@ import { SecondComponent } from './second.component';
 import { HttpServiceService } from '../http-service.service';
 import { HttpModule } from '@angular/http';
 
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 describe('SecondComponent', () => {
     let component: SecondComponent;
     let fixture: ComponentFixture<SecondComponent>;

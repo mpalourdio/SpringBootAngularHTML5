356f782253376b2a40dce977029dddc56b1208e9
Michel Palourdio <mpalourdio@gmail.com>
2017-07-10 20:40:43 +0200

    Add a test

---
+++ src/test/java/com/mpalourdio/html5/config/UselessTest.java
@@ -0,0 +1,15 @@
+package com.mpalourdio.html5.config;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UselessTest {
+
+    @Test
+    public void testOneEqualsOne() {
+        Assert.assertEquals(1,1);
+    }
+}

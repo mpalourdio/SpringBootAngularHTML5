c6b7129a370c0dbace802438b56cc6baf7b197d1
Michel Palourdio <mpalourdio@gmail.com>
2017-07-19 23:37:28 +0200

    Run tests with ChromiumHeadless

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -8,15 +8,16 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-phantomjs-launcher'),
+            require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
             require('@angular/cli/plugins/karma')
         ],
-        client:{
+        client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            reports: [ 'html', 'lcovonly' ],
+            reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
         },
         angularCli: {
@@ -27,7 +28,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['PhantomJS'],
+        browsers: ['ChromiumHeadless', 'PhantomJS'],
         singleRun: false
     });
 };

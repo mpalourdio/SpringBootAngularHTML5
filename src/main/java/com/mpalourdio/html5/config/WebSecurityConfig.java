6e042d96f5434366f643400aa222e1915482845e
Michel Palourdio <mpalourdio@gmail.com>
2017-10-31 08:15:43 +0100

    Remove configure(WebSecurity web)

--- src/main/java/com/mpalourdio/html5/config/WebSecurityConfig.java
+++ src/main/java/com/mpalourdio/html5/config/WebSecurityConfig.java
@@ -2,17 +2,11 @@ package com.mpalourdio.html5.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

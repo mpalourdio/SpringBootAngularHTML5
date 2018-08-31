1cd975351f680bdf1523e798c85d489e2905bfc3
Michel Palourdio <mpalourdio@gmail.com>
2018-08-31 21:12:42 +0200

    antMatchers ignore on /static

--- src/main/java/com/mpalourdio/html5/config/WebSecurityConfig.java
+++ src/main/java/com/mpalourdio/html5/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ package com.mpalourdio.html5.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -20,6 +21,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/static/**");
     }
 }
 
